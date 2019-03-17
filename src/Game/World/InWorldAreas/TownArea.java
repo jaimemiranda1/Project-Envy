98d5b5f9050d4b22f82464ff46c4c5b7af6a1fe9
jluis <jluis@192.168.1.11>
2019-03-16 22:10:52 -0400

    Added walls in town area.

--- src/Game/World/InWorldAreas/TownArea.java
+++ src/Game/World/InWorldAreas/TownArea.java
@@ -82,8 +82,22 @@ public class TownArea extends BaseArea {
 
     private void AddWalls() {
 
-
-
+    	townWalls.add(new InWorldWalls(handler, 1601, 1099, 1188, 664, "Wall"));	//House with pond
+    	townWalls.add(new InWorldWalls(handler, 2947, 1099, 540, 662, "Wall"));		//House in bottom right corner
+    	townWalls.add(new InWorldWalls(handler, 0, 1939, 3682, 165, "Wall"));		//Bottom Corner
+    	townWalls.add(new InWorldWalls(handler, 3393, 1713, 289, 239, "Wall"));		//Right Corner
+    	townWalls.add(new InWorldWalls(handler, 3487, 1001, 195, 807, "Wall"));		//
+    	townWalls.add(new InWorldWalls(handler, 3487, 0, 195, 846, "Wall"));		//
+    	townWalls.add(new InWorldWalls(handler, 2111, 0, 1383, 752, "Wall"));		//House in top right corner
+    	townWalls.add(new InWorldWalls(handler, 0, 0, 1917, 922, "Wall"));			//House in top left
+    	townWalls.add(new InWorldWalls(handler, 0, 917, 254, 1187, "Wall"));		//Left Corner
+    	townWalls.add(new InWorldWalls(handler, 245, 1093, 249, 211, "Wall"));		//
+    	townWalls.add(new InWorldWalls(handler, 253, 1229, 119, 169, "Wall"));		//
+    	townWalls.add(new InWorldWalls(handler, 1917, 0, 197, 84, "Wall"));			//Bench on top
+    	townWalls.add(new InWorldWalls(handler, 491, 1213, 193, 53, "Wall"));		//Bench in park
+    	townWalls.add(new InWorldWalls(handler, 1115, 1131, 57, 59, "Wall"));		//Pole near park
+    	townWalls.add(new InWorldWalls(handler, 807, 1687, 377, 285, "Wall"));		//Trees near park on the bottom
+    	
     }
 
     @Override
